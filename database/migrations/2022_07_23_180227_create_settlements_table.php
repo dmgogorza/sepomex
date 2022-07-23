3d7d20e552d6dcb723b8e12c93fb4bbae6654664
Diego Gogorza <dmgogorza@gmail.com>
2022-07-23 15:31:47 -0300

    Add settlement

---
+++ database/migrations/2022_07_23_180227_create_settlements_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettlementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settlements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('zone', ['Urbano', 'Rural']);
+            $table->integer('cp');
+            $table->unsignedBigInteger('municipality_id');
+            $table->unsignedBigInteger('internal_id');
+            $table->unsignedBigInteger('type_id');
+            $table->timestamps();
+            $table->foreign('municipality_id')->references('id')->on('municipalities');
+            $table->foreign('type_id')->references('id')->on('settlement_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settlements');
+    }
+}
