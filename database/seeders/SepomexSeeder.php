e8c85ac8d2d7945359fe3cf22f932471292e4b4c
Diego Gogorza <dmgogorza@gmail.com>
2022-07-25 18:54:08 -0300

    Add import seeders

--- database/seeders/SepomexSeeder.php
+++ database/seeders/SepomexSeeder.php
@@ -13,7 +13,29 @@ class SepomexSeeder extends Seeder
      */
     public function run()
     {
-
+        $this->call(ImportAguascalientesSeeder::class);
+        $this->call(ImportBajaCaliforniaSeeder::class);
+        $this->call(ImportBajaCaliforniaSurSeeder::class);
+        $this->call(ImportCampecheSeeder::class);
+        $this->call(ImportChiapasSeeder::class);
+        $this->call(ImportChihuahuaSeeder::class);
+        $this->call(ImportCiudadDeMexicoSeeder::class);
+        $this->call(ImportCoahuilaDeZaragozaSeeder::class);
+        $this->call(ImportColimaSeeder::class);
+        $this->call(ImportDurangoSeeder::class);
+        $this->call(ImportGuanajuatoSeeder::class);
+        $this->call(ImportGuerreroSeeder::class);
+        $this->call(ImportHidalgoSeeder::class);
+        $this->call(ImportJaliscoSeeder::class);
+        $this->call(ImportMexicoSeeder::class);
+        $this->call(ImportMichoacanDeOcampoSeeder::class);
+        $this->call(ImportMorelosSeeder::class);
+        $this->call(ImportNayaritSeeder::class);
+        $this->call(ImportNuevoLeonSeeder::class);
+        $this->call(ImportOaxacaSeeder::class);
+        $this->call(ImportPueblaSeeder::class);
+        $this->call(ImportQueretaroSeeder::class);
+        $this->call(ImportQuintanaRooSeeder::class);
         $this->call(ImportSanLuisPotosiSeeder::class);
         $this->call(ImportSinaloaSeeder::class);
         $this->call(ImportSonoraSeeder::class);
